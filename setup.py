ae74f4c5e89b5ab938db556fce536534738607a4
Per Hogfeldt <hogfeldt38@gmail.com>
2020-07-09 17:45:14 +0200

    established communication from cli to rendered graph

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     install_requires=[
         'click',
         'gwf>=1.7.2',
+        'graphviz',
     ],
     
     classifiers=[
--- src/gwf_graph/main.py
+++ src/gwf_graph/main.py
@@ -1,2 +1,21 @@
-def graph():
-    pass
+import click
+from graphviz import Digraph
+
+from gwf.core import Graph
+
+@click.command()
+@click.pass_obj
+def graph(obj):
+    graph = Graph.from_config(obj)
+    print(graph)
+    dot = Digraph(comment='The Round Table')
+    
+    dot.node('A', 'King Arthur')
+    dot.node('B', 'Sir Bedevere the Wise')
+    dot.node('L', 'Sir Lancelot the Brave')
+
+    dot.edges(['AB', 'AL'])
+    dot.edge('B', 'L', constraint='false')
+    dot.render('test-output/round-table.gv', view=True)
+
+
