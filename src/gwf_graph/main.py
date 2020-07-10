f016c747cc95205590ca7db60d0b279c81289daf
Per Hogfeldt <hogfeldt38@gmail.com>
2020-07-10 19:43:52 +0200

    Added comment and possible exception raise

--- src/gwf_graph/main.py
+++ src/gwf_graph/main.py
@@ -3,6 +3,9 @@ from graphviz import Digraph
 
 from gwf.core import Graph
 from gwf.filtering import filter_names
+from gwf.exceptions import GWFError
+
+first = lambda p: p[0]
 
 @click.command()
 @click.argument('targets', nargs=-1)
@@ -10,9 +13,18 @@ from gwf.filtering import filter_names
 def graph(obj, targets):
     graph = Graph.from_config(obj)
 
+    # If targets supplyed only show dependencies for thoes targets
+    # otherwise show the whole workflow
     matches = graph.targets.values()
     if targets:
         matches = filter_names(matches, targets)
+        # TODO: Consider if the check below is useful, it would make sure
+        #       that no empty pdf is created.
+        #
+        # Check for non existing targets
+        #for target in targets:
+        #    if target not in matches:
+        #        raise GWFError(f'No target named {target} found in workflow')
 
     dot = Digraph(comment='Dependency Graph')
     visited = set()
