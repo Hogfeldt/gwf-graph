793d6332281e0a6669ed6a7c59e422624b3ac352
Per Hogfeldt <hogfeldt38@gmail.com>
2020-07-10 18:45:43 +0200

    Targets argument added

--- src/gwf_graph/main.py
+++ src/gwf_graph/main.py
@@ -2,16 +2,29 @@ import click
 from graphviz import Digraph
 
 from gwf.core import Graph
+from gwf.filtering import filter_names
 
 @click.command()
+@click.argument('targets', nargs=-1)
 @click.pass_obj
-def graph(obj):
+def graph(obj, targets):
     graph = Graph.from_config(obj)
+
+    matches = graph.targets.values()
+    if targets:
+        matches = filter_names(matches, targets)
+
     dot = Digraph(comment='Dependency Graph')
-    for node in graph.targets.keys():
-        target = graph.targets[node]
-        name = target.name
-        dot.node(name, name)
-        for dep_target in graph.dependents[target]:
-            dot.edge(name, dep_target.name)
+    visited = set()
+    for root in matches:
+        if root in visited:
+            continue
+        for target in graph.dfs(root):
+            name = target.name
+            if name in visited:
+                continue
+            dot.node(name, name)    #shape='parallelogram'
+            for dep_target in graph.dependencies[target]:
+                dot.edge(name, dep_target.name)
+            visited.add(name)
     dot.render('dependency_graph.gv')
