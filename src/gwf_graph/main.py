1c2fdc0ff8bd9e17bc4491166caaeaa6d4bb32fe
Per Hogfeldt <hogfeldt38@gmail.com>
2020-07-10 06:06:12 +0200

    Basic functionality added

--- src/gwf_graph/main.py
+++ src/gwf_graph/main.py
@@ -7,15 +7,11 @@ from gwf.core import Graph
 @click.pass_obj
 def graph(obj):
     graph = Graph.from_config(obj)
-    print(graph)
-    dot = Digraph(comment='The Round Table')
-    
-    dot.node('A', 'King Arthur')
-    dot.node('B', 'Sir Bedevere the Wise')
-    dot.node('L', 'Sir Lancelot the Brave')
-
-    dot.edges(['AB', 'AL'])
-    dot.edge('B', 'L', constraint='false')
-    dot.render('test-output/round-table.gv', view=True)
-
-
+    dot = Digraph(comment='Dependency Graph')
+    for node in graph.targets.keys():
+        target = graph.targets[node]
+        name = target.name
+        dot.node(name, name)
+        for dep_target in graph.dependents[target]:
+            dot.edge(name, dep_target.name)
+    dot.render('dependency_graph.gv')
