758636825c3b5db4372e8abfaf9ce6a07729c685
Per Hogfeldt <hogfeldt38@gmail.com>
2020-07-11 07:59:31 +0200

    added output_type option

--- src/gwf_graph/main.py
+++ src/gwf_graph/main.py
@@ -9,8 +9,9 @@ first = lambda p: p[0]
 
 @click.command()
 @click.argument('targets', nargs=-1)
+@click.option('--output-type', type=click.Choice(['graphviz', 'cytoscape']), default='graphviz')
 @click.pass_obj
-def graph(obj, targets):
+def graph(obj, targets, output_type):
     graph = Graph.from_config(obj)
 
     # If targets supplyed only show dependencies for thoes targets
@@ -22,17 +23,20 @@ def graph(obj, targets):
         if not matches:
             raise GWFError('Non of the targets was found in the workflow')
 
-    dot = Digraph(comment='Dependency Graph')
-    visited = set()
-    for root in matches:
-        if root in visited:
-            continue
-        for target in graph.dfs(root):
-            name = target.name
-            if name in visited:
+    if output_type == 'graphviz':
+        dot = Digraph(comment='Dependency Graph')
+        visited = set()
+        for root in matches:
+            if root in visited:
                 continue
-            dot.node(name, name)    #shape='parallelogram'
-            for dep_target in graph.dependencies[target]:
-                dot.edge(name, dep_target.name, arrowsize='.5')
-            visited.add(name)
-    dot.render('dependency_graph.gv')
+            for target in graph.dfs(root):
+                name = target.name
+                if name in visited:
+                    continue
+                dot.node(name, name)    #shape='parallelogram'
+                for dep_target in graph.dependencies[target]:
+                    dot.edge(name, dep_target.name, arrowsize='.5')
+                visited.add(name)
+        dot.render('dependency_graph.gv')
+    elif output_type == 'cytoscape':
+        pass
