842393e439e1ff8d9e9f827465e578e2f426b1f8
Per Hogfeldt <hogfeldt38@gmail.com>
2020-07-11 17:19:51 +0200

    Formatted with black

--- setup.py
+++ setup.py
@@ -1,25 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='gwf-graph',
-    version='0.0.2',
+    name="gwf-graph",
+    version="0.0.2",
     author="Per Høgfeldt",
-    description='Create a visual representation of the dependency graph in your gwf workflow',
-
+    description="Create a visual representation of the dependency graph in your gwf workflow",
     packages=find_packages("src"),
     package_dir={"": "src"},
-
-    entry_points={
-        'gwf.plugins': ['graph = gwf_graph.main:graph']
-    },
-
+    entry_points={"gwf.plugins": ["graph = gwf_graph.main:graph"]},
     python_requires=">=3.6",
-    install_requires=[
-        'click',
-        'gwf>=1.7.2',
-        'graphviz',
-    ],
-    
+    install_requires=["click", "gwf>=1.7.2", "graphviz",],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
--- src/gwf_graph/main.py
+++ src/gwf_graph/main.py
@@ -9,10 +9,10 @@ from gwf.core import Graph
 from gwf.filtering import filter_names
 from gwf.exceptions import GWFError
 
-first = lambda p: p[0]
 
 def dfs(graph, root, visited={}):
     path = []
+
     def dfs_inner(node):
         if node in visited:
             return
@@ -20,8 +20,10 @@ def dfs(graph, root, visited={}):
         for dep in graph.dependencies[node]:
             dfs_inner(dep)
         path.append(node)
+
     dfs_inner(root)
     return path
+
 
 def visit_all_dependencies(graph, matches):
     visited = set()
@@ -29,9 +31,12 @@ def visit_all_dependencies(graph, matches):
     for target in chain(*paths):
         yield target
 
+
 @click.command()
-@click.argument('targets', nargs=-1)
-@click.option('--output-type', type=click.Choice(['graphviz', 'cytoscape']), default='graphviz')
+@click.argument("targets", nargs=-1)
+@click.option(
+    "--output-type", type=click.Choice(["graphviz", "cytoscape"]), default="graphviz"
+)
 @click.pass_obj
 def graph(obj, targets, output_type):
     graph = Graph.from_config(obj)
@@ -43,21 +48,21 @@ def graph(obj, targets, output_type):
         matches = filter_names(matches, targets)
         # Prevent drawing an empty graph
         if not matches:
-            raise GWFError('Non of the targets was found in the workflow')
+            raise GWFError("Non of the targets was found in the workflow")
 
-    if output_type == 'graphviz':
-        dot = Digraph(comment='Dependency Graph')
+    if output_type == "graphviz":
+        dot = Digraph(comment="Dependency Graph")
         for target in visit_all_dependencies(graph, matches):
             name = target.name
-            dot.node(name, name)    #shape='parallelogram'
+            dot.node(name, name)  # shape='parallelogram'
             for dep_target in graph.dependencies[target]:
-                dot.edge(name, dep_target.name, arrowsize='.5')
-        dot.render('dependency_graph.gv')
-    elif output_type == 'cytoscape':
-        with open('dependency_graph.sif', 'w') as fp:
-            writer = csv.writer(fp, delimiter=' ')
+                dot.edge(name, dep_target.name, arrowsize=".5")
+        dot.render("dependency_graph.gv")
+    elif output_type == "cytoscape":
+        with open("dependency_graph.sif", "w") as fp:
+            writer = csv.writer(fp, delimiter=" ")
             for target in visit_all_dependencies(graph, matches):
                 name = target.name
                 dependencies = list(map(lambda d: d.name, graph.dependencies[target]))
                 if dependencies:
-                    writer.writerow([name, 'dependencies'] + dependencies)
+                    writer.writerow([name, "dependencies"] + dependencies)
